30ddb5916b534609b2ef38fedc92033781dc48c6
rkdrud34 <rkdrud34@nate.com>
2020-01-25 15:09:49 +0900

    django-polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,26 @@
+from datetime import datetime
+
 from django.contrib import admin
+from django.utils import timezone
+
 from polls.models import Question,Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
